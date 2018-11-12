a5582b9cd89ad2216d14aabd4c27f56160c9802b
Emmanuel ZONDA <ezonda@e1r7p8.42.fr>
2018-11-12 18:04:20 +0100

    itoa_long2

--- rendu/ft_strncmp.c
+++ rendu/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ezonda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 17:56:09 by ezonda            #+#    #+#             */
-/*   Updated: 2018/11/12 16:58:14 by ezonda           ###   ########.fr       */
+/*   Updated: 2018/11/12 18:02:22 by ezonda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
+	size_t j;
 
 	i = 0;
+	j = 0;
+	while (s1[j] && s2[j])
+		j++;
 	while ((s1[i]) && (s2[i]) && (s1[i] == s2[i]) && (i < n))
 		i++;
+	if (j > n)
+		i--;
 	i = (unsigned char)s1[i] - (unsigned char)s2[i];
 	return (i);
 }
